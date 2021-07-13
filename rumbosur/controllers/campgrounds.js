ea5d2061e68b14ba3c2bf1dfb597dd37ca2b5f49
Molpo99 <83677729+Molpo99@users.noreply.github.com>
2021-07-13 09:38:21 -0300

    Add files via upload

---
+++ rumbosur/controllers/campgrounds.js
@@ -0,0 +1,101 @@
+const Campground = require('../models/campground');
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+const { cloudinary } = require("../cloudinary");
+
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({}).populate('popupText');
+    res.render('campgrounds/index', { campgrounds })
+}
+
+module.exports.findById = async (req,res) => {
+    const form = document.querySelector('#searcher');
+    form.addEventListener('submit', async function (e) {
+        e.preventDefault();
+        const searchTerm = form.elements.query.value;
+        const config = { params: { q: searchTerm } }
+        const campground = await Campground.findById(req.params.id);
+        const res = await axios.get(`${campground.title}`, config);
+        makeImages(res.data)
+        form.elements.query.value = '';
+    })
+    
+    const makeImages = (campgrounds) => {
+        for (let campground of campgrounds) {
+            if (campground.show.image) {
+                const img = document.createElement('IMG');
+                img.src = campground.show.image.medium;
+                document.body.append(img)
+            }
+        }
+    }
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('campgrounds/new');
+}
+
+module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
+    const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.author = req.user._id;
+    await campground.save();
+    console.log(campground);
+    req.flash('success', 'Se ha creado un nuevo destino!');
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.showCampground = async (req, res,) => {
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if (!campground) {
+        req.flash('error', 'No puedo encontrar ese destino!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id)
+    if (!campground) {
+        req.flash('error', 'No puedo encontrar ese destino!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+}
+
+module.exports.updateCampground = async (req, res) => {
+    const { id } = req.params;
+    console.log(req.body);
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
+    req.flash('success', 'Destino actualizado!');
+    res.redirect(`/campgrounds/${campground._id}`)
+}
+
+module.exports.deleteCampground = async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Destino eliminado!')
+    res.redirect('/campgrounds');
+}
---
+++ rumbosur/public/javascripts/search.js
@@ -0,0 +1,4 @@
+// const campground = require("../models/campground");
+// const express = require('express');
+
+
